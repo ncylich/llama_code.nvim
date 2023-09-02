1d9ef256f66d65bbd826234911404b0350508378
Noah Cylich <noahcylich@Noahs-MacBook-Pro.local>
2023-09-02 12:58:55 -0400

    popup added

--- lua/llama_code_plugin/code_llama_module.lua
+++ lua/llama_code_plugin/code_llama_module.lua
@@ -4,48 +4,57 @@ function code_llama_module.greeting(name)
    return "Hello " .. name
 end
 
-local base_llama_call = "/Users/noahcylich/LLAMA2/llama.cpp/main -m ./models/7B/ggml-model-q4_0.bin"
+LLAMA_CALL = "/Users/noahcylich/LLAMA2/llama.cpp/main -m ./models/7B/ggml-model-q4_0.bin"
 
-local suggested_code = nil
+local suggested_code, curr_code = nil, nil
 
 local function get_file_type()
   return vim.api.nvim_buf_get_option(0, 'filetype')
 end
 
 local function llama_request(request)
-  local cmd = base_llama_call.."-p"..request
+  local cmd = LLAMA_CALL.."-p"..request
   return vim.fn.system(cmd)
 end
 
-local function filter(id, key, options)
-  key = string.lower(key)
-  if string.find(key, 'yes') or string.gsub(key, ' ', '') == 'y' then
-    return 1
-  else
-    return 0
-  end
-end
-
-local function callback(id, result)
-  if result == 1 and suggested_code ~= nil then
+local function update()
+  if suggested_code ~= nil and curr_code ~= nil then
     vim.api.nvim_set_current_line(suggested_code)
-    suggested_code = nil
+    suggested_code, curr_code = nil, nil
   end
 end
 
 -- doesn't work: needs another plugin
-local function create_popup(curr_code)
-  if suggested_code ~= nil then
-    return vim.api.nvim_call('popup_create', ['current code: '..curr_code, 'suggested code: '..suggested_code], {'col': 1, 'padding': [1, 1, 1,], 'filter': filter, 'callback': callback})
-  end
+local function create_popup()
+  local popup = require('popup')
+
+  local lines = {
+    'current code: '..curr_code,
+    'suggested code: '..suggested_code,
+  }
+
+  local opts = {
+    title = 'Code Llama Suggestion',
+    line = 10,
+    -- col = 20,
+    border = true
+  }
+
+  local items = {
+    'Implement',
+    'Disregard',
+    {text = 'Option 3', code = 'update()'}
+  }
+
+  local win_id, win_opts = popup.menu(lines, opts, items)
 end
 
 function code_llama_module.llama_suggest()
   local language = get_file_type()
-  local curr_code = vim.api.nvim_get_current_line()
+  curr_code = vim.api.nvim_get_current_line()
   local request = 'Help complete the following code - the file ends in "'..language..'":\n'..curr_code
   suggested_code = llama_request(request)
-  return create_popup(curr_code)
+  return create_popup()
 end
 
 return code_llama_module
