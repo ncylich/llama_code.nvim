d438d8369e99784e15fbda4177502e4b67e37f5e
Noah Cylich <noahcylich@Noahs-MacBook-Pro.local>
2023-09-02 14:25:55 -0400

    adding llama_suggest through vim cmd line

--- lua/llama_code_plugin/code_llama_module.lua
+++ lua/llama_code_plugin/code_llama_module.lua
@@ -24,7 +24,8 @@ local function update()
   end
 end
 
--- doesn't work: needs another plugin
+-- popup.menu caused an error: field menu is nil?
+-- Try launching terminal instead of using popup?
 local function create_popup()
   local popup = require('popup')
 
@@ -49,12 +50,30 @@ local function create_popup()
   local win_id, win_opts = popup.menu(lines, opts, items)
 end
 
+local function check_response(response)
+  response = string.lower(response)
+  if string.find(response, 'yes') or 'y' == string.gsub(response, ' ', '') then
+    return true
+  end
+  return false
+end
+
+local function prompt_user()
+  print('Current Code: '..curr_code)
+  print('Suggested Code: '..suggested_code)
+  local response = vim.fn.input('Implement Suggestions? [y/N]')
+  if check_response(response) then
+    vim.api.nvim_set_current_line(suggested_code)
+  end
+  suggested_code, curr_code = nil, nil
+end
+
 function code_llama_module.llama_suggest()
   local language = get_file_type()
   curr_code = vim.api.nvim_get_current_line()
   local request = 'Help complete the following code - the file ends in "'..language..'":\n'..curr_code
   suggested_code = llama_request(request)
-  return create_popup()
+  prompt_user()
 end
 
 return code_llama_module
